bf0ec13cb183d65b905eb9c35f8c26d8273f9e5c
TheVerin <tomekbeben95@gmail.com>
2021-04-18 15:51:02 +0200

    Init configuration

---
+++ src/core/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+    openapi.Info(title="Taxlio", default_version="v1"),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+
+if settings.DEBUG:
+    urlpatterns.append(
+        path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger")
+    )
